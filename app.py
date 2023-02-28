dad541f70f4a2a6a3df3e0db5ba273d361ad2fe6
Ritwick Roy <37195857+2faced0w0@users.noreply.github.com>
2023-02-28 21:39:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 import streamlit as st
+import os
 
-st.header("Hello Data Science Internship 2023! ")
+st.title(":red[Innomatics] Data :blue[App] :sunglasses:")
+st.snow()
+
+btn_click = st.button("Don't click Me!")
+
+if btn_click==True:
+    st.subheader("You clicked the me :cry:")
+    st.balloons()
+    reset = st.button("Reset")
+
+
+
+
+
+"""st.header("Data Science Internship")
+
+st.subheader("Feb 2023")
+
+CODE = '''print("Hello World")'''
+
+st.code(CODE, language="python")"""
